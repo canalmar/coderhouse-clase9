3c7c57733409910f5d52f3a8afc75d0167c357f2
canalmar <marisatcanale@gmail.com>
2025-05-24 20:31:45 -0300

    agrego vistas y plantillas de pag inicio y nuevas rutas

--- core/views.py
+++ core/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def index(request):
+    context = {
+        "nombre": "vero",
+        "rol": "tutora"
+    }
+    return render(request,"core/index.html", context)
+
 def saludar(request):
     return HttpResponse("Hola desde Django!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1 style='color:red'>HOLA</h1>")
+
+def saludar_con_parametros(request,nombre:str, apellido:str):
+    nombre = nombre.capitalize()
+    apellido = apellido.upper()
+    return HttpResponse(f"{apellido}, {nombre}")
+
